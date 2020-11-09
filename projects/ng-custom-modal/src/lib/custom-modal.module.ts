93964dbd594d48b7b399eed5c982ac93c31d20b0
GersonUmanzor <gbumanzor@outlook.com>
2020-11-08 21:58:29 -0600

    :sparkles: Update version tag.

--- projects/ng-custom-modal/src/lib/custom-modal.module.ts
+++ projects/ng-custom-modal/src/lib/custom-modal.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { ModalComponent } from './components/modal/modal.component';
 import { ModalInsertionDirective } from './directives/modal-insertion.directive';
 import { CommonModule } from '@angular/common';
-import { ModalService } from 'ng-custom-modal';
+import { ModalService } from './services/modal.service';
 
 @NgModule({
   declarations: [ModalComponent, ModalInsertionDirective],
