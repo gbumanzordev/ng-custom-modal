28871d08c970c7d7a8b760469012665ef96ebd56
GersonUmanzor <gbumanzor@outlook.com>
2020-11-08 21:17:26 -0600

    :sparkles: Create modal service.

---
+++ projects/ng-custom-modal/src/lib/services/modal.service.ts
@@ -0,0 +1,82 @@
+import {
+  ApplicationRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  EmbeddedViewRef,
+  Injectable,
+  Injector,
+  Type,
+} from '@angular/core';
+import { ModalComponent } from '../components/modal/modal.component';
+import { ModalConfig } from '../utils/modal-config';
+import { ModalRef } from '../utils/modal-ref';
+import { ModalInjector } from '../utils/modal-injector';
+
+@Injectable()
+export class ModalService {
+  dialogComponentRef: ComponentRef<ModalComponent>;
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector
+  ) {}
+
+  open<ComponentType, ModalDataType = unknown>(
+    componentType: Type<ComponentType>,
+    config: ModalConfig<ModalDataType>
+  ): ModalRef {
+    const dialogRef = this.appendDialogComponentToBody<ModalDataType>(config);
+    this.dialogComponentRef.instance.setChildComponentType<ComponentType>(
+      componentType
+    );
+
+    if (config.width) {
+      this.dialogComponentRef.instance.width = config.width;
+    }
+
+    if (config.height) {
+      this.dialogComponentRef.instance.height = config.height;
+    }
+
+    return dialogRef;
+  }
+
+  close(): void {
+    this.removeDialogComponentFromBody();
+  }
+
+  private appendDialogComponentToBody<T>(config: ModalConfig<T>): ModalRef {
+    const map = new WeakMap();
+    map.set(ModalConfig, config);
+
+    const dialogRef = new ModalRef();
+    map.set(ModalRef, dialogRef);
+
+    const sub = dialogRef.afterClosed.subscribe(() => {
+      this.removeDialogComponentFromBody();
+      sub.unsubscribe();
+    });
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+      ModalComponent
+    );
+    const componentRef = componentFactory.create(
+      new ModalInjector(this.injector, map)
+    );
+    this.appRef.attachView(componentRef.hostView);
+
+    const domElement = (componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
+    document.body.appendChild(domElement);
+    this.dialogComponentRef = componentRef;
+    this.dialogComponentRef.instance.onClose.subscribe(() =>
+      this.removeDialogComponentFromBody()
+    );
+    return dialogRef;
+  }
+
+  private removeDialogComponentFromBody(): void {
+    this.appRef.detachView(this.dialogComponentRef.hostView);
+    this.dialogComponentRef.destroy();
+  }
+}
