34293aa98fcb251198a77eda2b11592fe2ee42ed
GersonUmanzor <gbumanzor@outlook.com>
2020-11-08 21:18:12 -0600

    :sparkles: Create modal component.

---
+++ projects/ng-custom-modal/src/lib/components/modal/modal.component.ts
@@ -0,0 +1,70 @@
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  OnDestroy,
+  Type,
+  ViewChild,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { ModalInsertionDirective } from '../../directives/modal-insertion.directive';
+import { ModalRef } from '../../utils/modal-ref';
+
+@Component({
+  selector: 'lib-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent implements AfterViewInit, OnDestroy {
+  childComponentType: Type<unknown>;
+  componentRef: ComponentRef<unknown>;
+  width = '50%';
+  height = '50%';
+  private readonly onClose$ = new Subject<unknown>();
+  public onClose = this.onClose$.asObservable();
+
+  @ViewChild(ModalInsertionDirective) insertionPoint: ModalInsertionDirective;
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private changeDetectorRef: ChangeDetectorRef,
+    private dialogRef: ModalRef
+  ) {}
+
+  setChildComponentType<T>(componentType: Type<T>): void {
+    this.childComponentType = componentType;
+  }
+
+  ngAfterViewInit(): void {
+    this.loadChildComponent(this.childComponentType);
+    this.changeDetectorRef.detectChanges();
+  }
+
+  onOverlayClicked(event: MouseEvent): void {
+    event.stopPropagation();
+    this.dialogRef.close();
+  }
+
+  loadChildComponent(componentType: Type<unknown>): void {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+      componentType
+    );
+
+    const viewContainerRef = this.insertionPoint.viewContainerRef;
+    viewContainerRef.clear();
+
+    this.componentRef = viewContainerRef.createComponent(componentFactory);
+  }
+
+  ngOnDestroy(): void {
+    if (this.componentRef) {
+      this.componentRef.destroy();
+    }
+  }
+
+  onModalClick(event: MouseEvent): void {
+    event.stopPropagation();
+  }
+}
