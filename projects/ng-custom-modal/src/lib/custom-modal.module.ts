112fcfc374c8794638563575b7f8f9e8ca6cb2e7
GersonUmanzor <gbumanzor@outlook.com>
2020-11-08 21:51:06 -0600

    :page_facing_up: Update package json.

--- projects/ng-custom-modal/src/lib/custom-modal.module.ts
+++ projects/ng-custom-modal/src/lib/custom-modal.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { ModalComponent } from './components/modal/modal.component';
 import { ModalInsertionDirective } from './directives/modal-insertion.directive';
 import { CommonModule } from '@angular/common';
+import { ModalService } from 'ng-custom-modal';
 
 @NgModule({
   declarations: [ModalComponent, ModalInsertionDirective],
   imports: [CommonModule],
   exports: [ModalComponent],
+  providers: [ModalService],
 })
 export class CustomModalModule {}
