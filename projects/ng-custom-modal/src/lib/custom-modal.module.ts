43ed7a057e1a2c1b5154cded2965ddbcf91102c7
GersonUmanzor <gbumanzor@outlook.com>
2020-11-21 14:59:55 -0600

    :recycle: Add directive to module.

--- projects/ng-custom-modal/src/lib/custom-modal.module.ts
+++ projects/ng-custom-modal/src/lib/custom-modal.module.ts
@@ -3,11 +3,12 @@ import { ModalComponent } from './components/modal/modal.component';
 import { ModalInsertionDirective } from './directives/modal-insertion.directive';
 import { CommonModule } from '@angular/common';
 import { ModalService } from './services/modal.service';
+import { ModalCloseDirective } from './directives/modal-close.directive';
 
 @NgModule({
-  declarations: [ModalComponent, ModalInsertionDirective],
+  declarations: [ModalComponent, ModalInsertionDirective, ModalCloseDirective],
   imports: [CommonModule],
-  exports: [ModalComponent],
+  exports: [ModalComponent, ModalCloseDirective],
   providers: [ModalService],
 })
 export class CustomModalModule {}
